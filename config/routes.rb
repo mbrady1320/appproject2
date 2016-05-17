c462d01e073afed6ca71b85b79daa28a4b7f6398
Melissa <mbrady1320@gmail.com>
2016-05-17 16:40:25 +0000

    edited navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
-  get 'about/' => 'static_pages#about'
+  
+  get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
 
