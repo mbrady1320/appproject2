4955d680a7a5a62364d74576725ac8b8750b501b
Melissa <mbrady1320@gmail.com>
2016-06-03 16:25:52 +0000

    defined search_term in products controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,12 @@ class ProductsController < ApplicationController
 
   # GET /products
   def index
+    search_term = params[:q]
     if params[:q]
       @products = Product.where("LOWER(name)LIKE? OR LOWER(description)LIKE?", "%#{search_term.downcase}%", "%#{search_term.downcase}")
     else
       @products = Product.all
-    end
+    end 
   end
   
 
