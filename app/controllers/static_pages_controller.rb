46b29bc861b149390ae5b88bc1e2edec72ca72bd
Melissa <mbrady1320@gmail.com>
2016-06-09 18:51:43 +0000

    added sendgrid app and configured ActionMailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,13 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
   
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'mbrady1320@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#about'
-
+  root 'static_pages#index'
+  
+  post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
