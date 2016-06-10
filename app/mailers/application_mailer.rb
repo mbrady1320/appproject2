cf9e011de17567d6a984630a9d92571860c8d527
Melissa <mbrady1320@gmail.com>
2016-06-10 19:55:32 +0000

    created mail template and preview

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,6 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "info@bikeberlin.com"
+  layout 'mailer'
+end
+
+
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  default from: "info@bikeberlin.com"
+  
+  def contact_form(email, name, message)
+  @message = message
+    mail(:from => email, :to => 'mbrady1320@gmail.com', :subject => "A new contact form message from #{name}")
+  end
+end
