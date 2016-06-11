a7841abff03e2ce64238ba11db8efce42edc84ea
Melissa <mbrady1320@gmail.com>
2016-06-11 00:44:22 +0000

    edited static_pages_controller and user_mailer.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,9 +14,6 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(:from => @email,
-      :to => 'mbrady1320@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: "info@bikeberlin.com"
   
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(:from => email, :to => 'mbrady1320@gmail.com', :subject => "A new contact form message from #{name}")
   end
 end
