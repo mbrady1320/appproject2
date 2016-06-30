21a0dc571c177dd7cfaf14c3bd903d9b1fa54101
Melissa <mbrady1320@gmail.com>
2016-06-30 01:51:13 +0000

    debugging Heroku broken show link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comment.order("created_at DESC")
+    @comments = @product.comments.order("created_at DESC")
   end
 
   # GET /products/new
