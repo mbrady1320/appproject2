5d3e4b74f6e69e60e24d965e0e98449aa126342e
Melissa <mbrady1320@gmail.com>
2016-07-14 23:23:56 +0000

    added first and last name params to devise

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -5,7 +5,14 @@ class UserRegistrationsController < Devise::RegistrationsController
       UserMailer.welcome(@user).deliver_now
     end
   end
-end
-private
+    
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+  
+end  
 
